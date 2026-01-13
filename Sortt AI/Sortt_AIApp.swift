1ea1903f32ea9233cfe970fb5fe1dab43e91dbfa
Integrate AI, LLC <connect@integrateai.llc>
2026-01-13 14:30:25 -0500

    Initial Commit

---
+++ Sortt AI/Sortt_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sortt_AIApp.swift
+//  Sortt AI
+//
+//  Created by Integrate AI on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Sortt_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
